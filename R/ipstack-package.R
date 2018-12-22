9554a3534d031d3123e19f4cc53dd8ce6531ccc4
boB Rudis <bob@rud.is>
2018-12-22 13:22:57 -0500

    R package repo initialization complete

---
+++ R/ipstack-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ipstack>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ipstack/issues>
+#' 
+#' @md
+#' @name ipstack
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
